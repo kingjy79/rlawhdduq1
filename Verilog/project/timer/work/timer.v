a05bdec94f7171872e2d722fe9e811e08306a66a
kingjy79 <kingjy0709@naver.com>
2018-07-21 22:31:57 +0900

    in cafe

--- Verilog/project/timer/work/timer.v
+++ Verilog/project/timer/work/timer.v
@@ -12,18 +12,24 @@ decoder_2bit dec0(
 	.i_we(i_we),
 	.o_decoder(enable_to_dcount));
 */
-reg [16-1:0] max_counter;
-reg [16-1:0] pwm_counter;
+reg [16-1:0] pwm_count;
+reg [16-1:0] pwm_count;
 reg [16-1:0] stop_count;
 
 
 //after compile this file,I will modify this parameter to optimizition.
 localparam S0=1'b0, S1=1'b1;
-reg c_state, n_state; //1bit(2state)
+reg [2-1:0] c_state, n_state; //2bit(4state)
 reg [16-1:0] inner_count;
 reg [7-1:0] cycle_count; //depending how much..100.
 reg [3-1:0] end_count;
 
+always @(*) begin //fetch? information from decoder.,combination curcuit
+	decoder_2bit dec0(
+		.i_addr(i_adder),
+		.i_we(i_we),
+		.o_decoder(enable_to_dcount));
+		end
 
 always @(posedge i_clk, negedge i_rst_n) begin 
 	if(!i_rst_n) begin //stop reset -> driving this module.
@@ -32,94 +38,131 @@ always @(posedge i_clk, negedge i_rst_n) begin
 		c_state<=S0;
 		n_state<=S0;
 		enable_to_dcount<=0;
-		max_counter<=0;
-		pwm_counter<=0;
+		pwm_count<=0;
+		pwm_count<=0;
 		stop_count<=0;
 		inner_count<=0;
 		cycle_count<=0;
 		end_count<=0;
 		end
 
-	else begin
-		d_ff dff_max #(N=16)(
-							.i_clk(i_clk)
-							.i_rst_n(i_rst_n)
-							.i_data(i_wdata)
-							.i_enable(enable_to_dcount[0])
-							.o_data(max_counter));
-		d_ff dff_pwm #(N=16)(
-							.i_clk(i_clk)
-							.i_rst_n(i_rst_n)
-							.i_data(i_wdata)
-							.i_enable(enable_to_dcount[0])
-							.o_data(max_counter));
-		d_ff dff_ #(N=16)(
-							.i_clk(i_clk)
-							.i_rst_n(i_rst_n)
-							.i_data(i_wdata)
-							.i_enable(enable_to_dcount[0])
-							.o_data(max_counter));
-		c_state<=n_state; //while Driving
-		case(c_state)//this state maintain before posedge.
-  			S0:begin //initial state
-  				if(i_start==1) begin//S0->S1
-      				if(enable_to_dcount==4'h1) begin
-           				max_counter=i_wdata;
+	else begin//I wanted to use module d_ff, but it is expected delay 2 clocks.
+      	
+       	c_state<n_state;
+		case(c_state)
+       		S0:begin
+       			if(enable_to_dcount==4'h1) begin//0001
+           			pwm_count=i_wdata;
+        			end
+      			else if(enable_to_dcount==4'h2) begin//0010
+          			pwm_count=i_wdata;
+          			end
+      			else if(enable_to_dcount==4'h4) begin//0100
+        			stop_count=i_wdata;
+        			end
+    		   	else begin
+      		 		pwm_count=pwm_count;
+       				pwm_count=pwm_count;
+       				stop_count=stop_count;
+       				end  
+       			if(i_start==1) begin
+       				//S0->S1 Action description
+       				if(cycle_count<stop_cycle) begin
+        				inner_count<=inner_count+1;
+        				if(inner_count<pwm_count) begin
+        					o_pwm<=0;
+        					end
+        				else if(pwm_count<=inner_count && inner_count<max_count) begin
+        					o_pwm<=1;
+        				else begin//max_count<=inner_count
+        					inner_count<=0;//initialization inner_count
+        					cycle_count<=cycle_count+1;//when 'max_count' and 'inner_count' are equal, 'cycle_count' is incremented by one.
+        					end
+        			else if(stop_count<=cycle_count) begin// && cycle_count<(stop_count+10)) begin
+        				o_timer_end<=1;
         				end
-      				else if(enable_to_dcount==4'h2) begin
-          				pwm_counter=i_wdata;
-          				end
-      				else if(enable_to_dcount==4'h4) begin
-        				stop_count=i_wdata;
+        			n_state<=S1;
+        			else begin
+        				n_state<=S1; //to avoid creating a latch
         				end
-        			else begin//after fetch from i_wdata.
-        				n_state=S1;//S0->S1 
-        				end   					
-   				else//i_start==0, S0->S0
-      				n_state=S0;
-        			end    	   
-			default begin//S1
-				n_state=S0; //S1->S0
-    			end
-      		endcase
-		inner_count<=inner_count+1;//inner_count is operated for o_pwm.
-		if(stop_count>=cycle_count) begin//cycle_count comfirms how many times stop_count counts.
-		//it is still in progress. output is not affected by i_start. 
-			if(end_count<10) begin //end_count is drived for o_time_end maintain value 1. 
-				o_time_end<=1;
-				end_count=end_count+1;
-				end
-			else begin
-				o_time_end<=0;
-				end
+        			end
+				else begin
+        		//S0->S0 action description
+        			n_state<=S0;
+        			end
+        		end
+        			/*
+        			else begin//(stop_count+10)<=cycle_count
+        				o_timer_end<=0;
+        				cycle_count<=0; //when 'stop_count+10' and 'cycle_count are equal, set cycle_count to 0	
+        				end
+        			*/
+        	S1:begin
+       			if(o_timer_end==0) begin
+       				//S1->S1 Action description
+       				if(cycle_count<stop_cycle) begin
+        				inner_count<=inner_count+1;
+        				if(inner_count<pwm_count) begin
+        					o_pwm<=0;
+        					end
+        				else if(pwm_count<=inner_count && inner_count<max_count) begin
+        					o_pwm<=1;
+        				else begin//max_count<=inner_count
+        					inner_count<=0;//initialization inner_count
+        					cycle_count<=cycle_count+1;//when 'max_count' and 'inner_count' are equal, 'cycle_count' is incremented by one.
+        					end
+        			else if(stop_count<=cycle_count && cycle_count<(stop_count+10)) begin
+        				o_timer_end<=1;
+        				end
+        			else begin
+        				n_state<=S1;
+        				end
+        			n_state<=S1;
+        			end
+        		else begin//o_timer_end==1
+        		//action description: S1->S2
+        			if(cycle_count<(stop_count+10)) begin
+        				o_timer_end<=o_time_end;
+        				cycle_count<=cycle_count+1; //when 'stop_count+10' and 'cycle_count are equal, set cycle_count to 0	
+        				end
+        			else//stop_count+10<=cycle_count
+        				o_timer_end<=0;
+        				cycle_count<=0; //when 'stop_count+10' and 'cycle_count are equal, set cycle_count to 0	
+        				end
+					n_state<=S2;
+        			end
+        		end
+        	S2:begin
+        		if(o_timer_end==1) begin
+        		//action description S2->S2
+        			if(cycle_count<(stop_count+10)) begin
+        				o_timer_end<=o_time_end;
+        				cycle_count<=cycle_count+1; //when 'stop_count+10' and 'cycle_count are equal, set cycle_count to 0	
+        				end
+        			else//stop_count+10<=cycle_count
+        				o_timer_end<=0;
+        				cycle_count<=0; //when 'stop_count+10' and 'cycle_count are equal, set cycle_count to 0	
+        				end
+					n_state<=S2;
+        			end
+        		else begin//o_timer_end==0
+        		
+        			n_state<=S0;
+        			end
+        		end
+        		else begin
+        		//action description S2->S0
 
-			end
-			
-			
-		if(c_state==S1) begin
-			if(pwm_count<=inner_count && inner_count<max_count) begin
-				o_pwm=1;//In special range, It will have value 1 
-				end
-			else if(inner_count<pwm_count) begin
-				o_pwm=0;
-				end
-			else //inner_count>=max_count
-				cycle_count<=cycle_count+1;
-			if(stop_count==cycle_count) begin
-				o_timer_end<=1;
-		end
-	end
-
-always @(*) begin //fetch? information from decoder. 
-	decoder_2bit dec0(
-		.i_addr(i_adder),
-		.i_we(i_we),
-		.o_decoder(enable_to_dcount));
-	end
-
-
-    	    
-
+        			n_state<=S0;
+        			end
+        		end
+        	default: begin
+        		n_state<=S0;
+        		end
+        	endcase
+        end
+	end		
 endmodule 
+
 
 
